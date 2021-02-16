7460360e42909fc4c6cb497d7d35572549225207
Alfred <ajsewell13@gmail.com>
2021-02-16 00:24:56 -0600

    added books page

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -6,13 +6,12 @@ const bookModel = require('../models/book');
 
 let dbStatus = db.connection.readyState;
 
-router.get('/books', function(req, res, next) {
-  res.redirect('/catalog/books');
-});
+router.get('/books', (req, res) => res.redirect('/catalog/books'));
 
 router.get('/catalog/books', async (req, res, next) => {
   let booksQuery = await bookModel.find({});
-  res.render('booksIndex', { title: 'Our Books', dbStatus: dbStatus, books: booksQuery });
+  let authorsMaybe = bookModel.find({}).populate('author');
+  res.render('booksIndex', { title: 'Our Books', dbStatus: dbStatus, books: booksQuery, authors: authorsMaybe });
 });
 
 module.exports = router;
