39dda0b218133aef9784a34f39bd180db7e449ff
Alfred <ajsewell13@gmail.com>
2021-02-27 14:13:57 -0600

    removed uri

--- db.js
+++ db.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
-var dev_db_url = "mongodb://alfred:CUHxmrmy3uGI1G25@cluster0-shard-00-00.fcpxz.mongodb.net:27017,cluster0-shard-00-01.fcpxz.mongodb.net:27017,cluster0-shard-00-02.fcpxz.mongodb.net:27017/local_library?ssl=true&replicaSet=atlas-10n3od-shard-0&authSource=admin&retryWrites=true&w=majority"
-
-var mongoDB = process.env.MONGO_URI || dev_db_url;
+var mongoDB = process.env.MONGO_URI
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
