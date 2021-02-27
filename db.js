4a28774af3b181aaf711d830a7fc2353f0bf9c38
Alfred <ajsewell13@gmail.com>
2021-02-27 14:07:42 -0600

    added production DB

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-var debug = require('debug')('Database');
+var dev_db_url = "mongodb://alfred:CUHxmrmy3uGI1G25@cluster0-shard-00-00.fcpxz.mongodb.net:27017,cluster0-shard-00-01.fcpxz.mongodb.net:27017,cluster0-shard-00-02.fcpxz.mongodb.net:27017/local_library?ssl=true&replicaSet=atlas-10n3od-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+var mongoDB = process.env.MONGO_URI || dev_db_url;
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+var debug = require('debug')('database');
 
 debug('database connection state: ' + mongoose.connection.readyState)
 
