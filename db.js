c0c36a17eedcc1ea37a5ab9fb2fb4d5c0f353f95
Alfred <ajsewell13@gmail.com>
2021-02-27 14:02:06 -0600

    changed console log to use debug package

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log('database connection state: ', mongoose.connection.readyState)
+var debug = require('debug')('Database');
+
+debug('database connection state: ' + mongoose.connection.readyState)
 
 module.exports = mongoose
